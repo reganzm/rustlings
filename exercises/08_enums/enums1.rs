bb5c5afc2f7ca68280ff027cdeab36a07f2990b9
zhangminyf <zhangminyf@wekoi.cn>
2025-07-21 18:27:05 +0800

    [add]06 07 08

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,12 +1,17 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+    Resize,
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 fn main() {
     println!("{:?}", Message::Resize);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Move { x: 10, y: 20 });
+    println!("{:?}", Message::Echo(String::from("Hello, world!")));
+    println!("{:?}", Message::ChangeColor(255, 0, 0));
     println!("{:?}", Message::Quit);
 }
