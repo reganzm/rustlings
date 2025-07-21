bb9b5103b50e9fe856fea7961d4f050235a0cb1d
奔跑的小米 <626692024@qq.com>
2025-07-21 22:56:33 +0800

    update

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -15,6 +15,9 @@ mod delicious_snacks {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
+
+    use self::fruits::PEAR as fruit;
+    use self::veggies::CUCUMBER as veggie;
 }
 
 fn main() {
