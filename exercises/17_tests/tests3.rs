b9336617cb91db9949328e4e9c055d30be6797ec
zhangminyf <zhangminyf@wekoi.cn>
2025-07-22 11:46:13 +0800

    update

--- exercises/17_tests/tests3.rs
+++ exercises/17_tests/tests3.rs
@@ -28,22 +28,32 @@ mod tests {
     fn correct_width_and_height() {
         // TODO: This test should check if the rectangle has the size that we
         // pass to its constructor.
+
         let rect = Rectangle::new(10, 20);
-        assert_eq!(todo!(), 10); // Check width
-        assert_eq!(todo!(), 20); // Check height
+        assert_eq!(rect.width, 10); // Check width
+        assert_eq!(rect.height, 20); // Check height
     }
 
     // TODO: This test should check if the program panics when we try to create
     // a rectangle with negative width.
     #[test]
     fn negative_width() {
-        let _rect = Rectangle::new(-10, 10);
+
+        // This will panic if the width is negative.
+        // We expect this test to fail if the panic doesn't happen. 
+        assert_panics!({
+            let _rect = Rectangle::new(-10, 10);
+        });
     }
 
     // TODO: This test should check if the program panics when we try to create
     // a rectangle with negative height.
     #[test]
     fn negative_height() {
-        let _rect = Rectangle::new(10, -10);
+        // This will panic if the height is negative.
+        // We expect this test to fail if the panic doesn't happen.
+        assert_panics!({
+            let _rect = Rectangle::new(10, -10);
+        });
     }
 }
